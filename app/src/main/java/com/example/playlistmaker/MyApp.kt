4580fe1cd617dc9f984cb0dc533dc96462886977
Andrei Urich <andrei.urich@yandex.ru>
2024-07-14 15:29:39 +0400

    исправил косяк по верстке на экране аудиоплейера ( наименование альбома) и почистил MyApp от ненужного кода

--- app/src/main/java/com/example/playlistmaker/MyApp.kt
+++ app/src/main/java/com/example/playlistmaker/MyApp.kt
@@ -1,9 +1,6 @@
 package com.example.playlistmaker
 
 import android.app.Application
-import android.content.res.Configuration
-import android.os.Build
-import androidx.appcompat.app.AppCompatDelegate
 import com.example.playlistmaker.creator.Creator
 
 class MyApp : Application() {
