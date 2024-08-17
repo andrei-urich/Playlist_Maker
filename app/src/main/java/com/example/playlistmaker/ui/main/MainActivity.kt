b1412105ae19737b317d8bfa1fc1f997c1901d08
Andrei Urich <andrei.urich@yandex.ru>
2024-08-17 14:34:12 +0400

    Раскрасил BottomNavigationView

--- app/src/main/java/com/example/playlistmaker/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/main/MainActivity.kt
@@ -1,8 +1,21 @@
 package com.example.playlistmaker.ui.main
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bnView)
+        bottomNavView.setupWithNavController(navController)
+
+    }
 }
