a88b4bc7288e204567c67d2ba2d11d6c41e6fa74
Andrei Urich <andrei.urich@yandex.ru>
2024-10-07 18:51:30 +0400

    сверстал фрагмент для добавления нового плейлиста

---
+++ app/src/main/java/com/example/playlistmaker/presentation/library/CreatePlaylistViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.playlistmaker.presentation.library
+
+import androidx.lifecycle.ViewModel
+
+class CreatePlaylistViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/playlistmaker/ui/library/CreatePlaylistFragment.kt
@@ -0,0 +1,32 @@
+package com.example.playlistmaker.ui.library
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.playlistmaker.R
+import com.example.playlistmaker.presentation.library.CreatePlaylistViewModel
+
+class CreatePlaylistFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CreatePlaylistFragment()
+    }
+
+    private val viewModel: CreatePlaylistViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_create_playlist, container, false)
+    }
+}
