e7768625d523b6eeea627775f2b2d298a332db0c
Andrei Urich <andrei.urich@yandex.ru>
2024-10-17 15:26:26 +0400

    кнопку "удалить плейлисты" убрал (делал для тестирования)

--- app/src/main/java/com/example/playlistmaker/ui/library/PlaylistsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/library/PlaylistsFragment.kt
@@ -57,13 +57,6 @@ class PlaylistsFragment : Fragment() {
             viewModel.createPlaylist()
         }
 
-
-        val clearBtn = binding.bnDellaylist
-        clearBtn.setOnClickListener {
-            viewModel.clearPlaylists()
-            viewModel.getPlaylistList()
-        }
-
         viewModel.getLiveData().observe(viewLifecycleOwner) { list ->
             binding.progressBar.visibility = View.GONE
             showPlaceholder(list.isNullOrEmpty())
